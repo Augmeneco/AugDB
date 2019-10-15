553210137fcfb746f194cf8c6718ead140dc1fb3
Cha14ka <taitor@yandex.ru>
2019-10-15 22:16:15 +0300

    Полный редизайн + сохранение в жсон файл

--- main.pas
+++ main.pas
@@ -6,21 +6,34 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Grids,
-  ExtCtrls, Types;
+  ExtCtrls, StdCtrls, Menus, SynCompletion, SynEdit, Types, jsonparser, fpjson;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    menu_close: TMenuItem;
+    menu_load: TMenuItem;
+    MainMenu1: TMainMenu;
+    Memo1: TMemo;
+    MenuItem1: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
     PageControl1: TPageControl;
     bd_main: TTabSheet;
     bd_edit: TTabSheet;
     bd_create: TTabSheet;
+    PageControl2: TPageControl;
     Panel1: TPanel;
+    menu_save: TMenuItem;
+    SaveDialog1: TSaveDialog;
     StringGrid1: TStringGrid;
-    procedure bd_mainContextPopup(Sender: TObject; MousePos: TPoint;
-      var Handled: Boolean);
+    TabSheet1: TTabSheet;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure menu_saveClick(Sender: TObject);
   private
 
   public
@@ -29,6 +42,11 @@ type
 
 var
   Form1: TForm1;
+  basedata: array of array of string;
+  gridslist: array of TStringGrid;
+  gridsnames: array of string;
+  jsonobj: TJSONObject;
+  col,row: integer;
 
 implementation
 
@@ -36,10 +54,58 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.bd_mainContextPopup(Sender: TObject; MousePos: TPoint;
-  var Handled: Boolean);
+procedure savebd();
+var
+  bd_id: integer;
+  json1,json2: TJSONObject;
+  json3: TJSONArray;
+begin
+  Form1.SaveDialog1.Title:='Сохранение базы данных';
+  Form1.SaveDialog1.InitialDir := GetCurrentDir;
+  Form1.SaveDialog1.Filter := 'json|*.json|AugDB|*.augdb|DataBase|*.db';
+  Form1.SaveDialog1.DefaultExt := 'augdb';
+  Form1.SaveDialog1.FilterIndex := 1;
+  if not Form1.SaveDialog1.Execute then Exit;
+
+  json1:= TJSONObject.Create;
+
+  for bd_id:=0 to Form1.PageControl2.PageCount-1 do
+  begin
+    json2 := TJSONObject.Create;
+    for col:=1 to gridslist[bd_id].ColCount-1 do
+    begin
+      json3 := TJSONArray.Create;
+      for row:=1 to gridslist[bd_id].RowCount-1 do
+      begin
+        json3.Add(gridslist[bd_id].Cells[col,row]);
+      end;
+      json2.Add(gridslist[bd_id].Cells[col,0],json3);
+    end;
+    json1.Objects[gridsnames[bd_id]] := json2;
+  end;
+  writeln(json1.FormatJSON);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Memo1.Text:='';
+  StringGrid1.ColCount:=1;
+  StringGrid1.RowCount:=1;
+  SetLength(gridslist,1);
+  SetLength(gridsnames,1);
+  gridslist[0] := StringGrid1;
+  gridsnames[0] := 'таблица1';
+  PageControl2.hide;
+end;
+
+procedure TForm1.menu_saveClick(Sender: TObject);
+begin
+  savebd();
 end;
 
 end.
