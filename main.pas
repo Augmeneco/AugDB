6c32243447eecf3c95a0c7e33cf10ddce5413fdf
Cha14ka <taitor@yandex.ru>
2019-10-16 02:13:16 +0300

    добавлено мелкое управление таблицей

--- main.pas
+++ main.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Grids,
-  ExtCtrls, StdCtrls, Menus, SynCompletion, SynEdit, Types, jsonparser, fpjson;
+  ExtCtrls, StdCtrls, Menus, ActnList, SynCompletion, SynEdit, Types,
+  jsonparser, fpjson;
 
 type
 
@@ -14,6 +15,10 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    edit_col_name: TEdit;
+    select_table: TComboBox;
     menu_close: TMenuItem;
     menu_load: TMenuItem;
     MainMenu1: TMainMenu;
@@ -25,15 +30,16 @@ type
     bd_main: TTabSheet;
     bd_edit: TTabSheet;
     bd_create: TTabSheet;
-    PageControl2: TPageControl;
     Panel1: TPanel;
     menu_save: TMenuItem;
     SaveDialog1: TSaveDialog;
     StringGrid1: TStringGrid;
-    TabSheet1: TTabSheet;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure menu_saveClick(Sender: TObject);
+    procedure select_tableChange(Sender: TObject);
   private
 
   public
@@ -69,7 +75,7 @@ begin
 
   json1:= TJSONObject.Create;
 
-  for bd_id:=0 to Form1.PageControl2.PageCount-1 do
+  for bd_id:=0 to Form1.select_table.Items.Count-1 do
   begin
     json2 := TJSONObject.Create;
     for col:=1 to gridslist[bd_id].ColCount-1 do
@@ -88,24 +94,41 @@ end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
+  select_table.Items.Add('таблица'+inttostr(select_table.Items.Count));
+end;
 
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  StringGrid1.ColCount:=StringGrid1.ColCount+1;
+  StringGrid1.Cells[StringGrid1.ColCount-1,0] := edit_col_name.Caption;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  StringGrid1.ColCount:=StringGrid1.ColCount-1;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   Memo1.Text:='';
   StringGrid1.ColCount:=1;
-  StringGrid1.RowCount:=1;
+  StringGrid1.RowCount:=2;
   SetLength(gridslist,1);
   SetLength(gridsnames,1);
   gridslist[0] := StringGrid1;
   gridsnames[0] := 'таблица1';
-  PageControl2.hide;
+  select_table.Items.Add('таблица1');
+  select_table.ItemIndex:=0;
 end;
 
 procedure TForm1.menu_saveClick(Sender: TObject);
 begin
   savebd();
+end;
+
+procedure TForm1.select_tableChange(Sender: TObject);
+begin
+  ShowMessage(inttostr(select_table.ItemIndex));
 end;
 
 end.
